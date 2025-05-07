55bc55152f6003818eb75c7948e8857e0f175d96
Simone <simone.brunelli3@studio.unibo.it>
2025-05-07 16:32:09 +0200

    InitSetup

---
+++ build.gradle.kts
@@ -0,0 +1,32 @@
+plugins {
+    java
+    application
+    id("org.danilopianini.gradle-java-qa") version "1.77.0"
+}
+
+group = "it.unibo.exam"
+version = "1.0-SNAPSHOT"
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+    // Aggiungi eventuali dipendenze qui
+}
+
+application {
+    mainClass.set("it.unibo.exam.Main")
+}
+
+tasks.withType<JavaCompile> {
+    options.encoding = "UTF-8"
+}
+
+sourceSets {
+    main {
+        java {
+            setSrcDirs(listOf("src/main/java"))
+        }
+    }
+}
